410136e75959f2642d24a3bc0efd6c0d69dd587d
RemidelaKethulledeRyhove-r0624876 <remidelakethullederyhove@student.ucll.be>
2019-02-26 12:42:43 +0100

    aanpassingen

--- js/heatCalendar.js
+++ js/heatCalendar.js
@@ -1,5 +1,12 @@
 var cal = new CalHeatMap();
 	cal.init({  
         id: "#cal-heatmap",
-        data: "datafiles/dummyData.json"
+        data: "datafiles/dummyData.json",
+        domain : "day",
+		subDomain : "hour",
+        range: 14,
+        cellSize: 20,
+        legendCellSize: 20,
+        itemName: ["insuline"],
+        legend: [100, 200]
     });
