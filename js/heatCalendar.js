b5c924c83a4c47f1444b3ebc15f8d54604ce2981
RemidelaKethulledeRyhove-r0624876 <remidelakethullederyhove@student.ucll.be>
2019-02-27 15:07:19 +0100

    data toont eindelijk op kalender

--- js/heatCalendar.js
+++ js/heatCalendar.js
@@ -1,29 +1,52 @@
-var cal = new CalHeatMap();
-	cal.init({  
+function init(stats) {
+    //console.log(Object.values(stats) + "help");
+    var whut = JSON.stringify(stats);
+    //console.log(whut+ "help3");
+    var cal = new CalHeatMap();
+    cal.init({
         id: "#cal-heatmap",
-        data: "datafiles/dummyData.json",
-        domain : "month",
-		subDomain : "day",
+        data: stats,
+        dataType: "txt",
+        domain: "month",
+        start: new Date(2019, 0, 26),
         cellSize: 20,
         legendCellSize: 20,
-        itemName: ["insuline"],
+        itemName: ["glucose"],
         legend: [100, 200],
         colLimit: 31,
         verticalOrientation: true,
         label: {
-		position: "left",
-	}
+            position: "left",
+        },
     });
+}
+setData();
 
-d3.json("datafiles/dummyData.json", function (data) {
-    var date;
-             data.forEach(function (d) {
-                 console.log(d.Tijd + "test");
-                 d.Tijd += ":00";
-                 console.log(d.Tijd + "test3");
-                 var date = Math.round(new Date(d.Tijd).getTime()/1000)
-                 console.log(date+"whuut");
-                 d.glucose = d["Historie glucose (mg/dL)"];
-                 console.log(d.glucose + "test2");
-             });
-         })
+function setData() {
+    var stats = {};
+
+    d3.json("datafiles/dummyData.json", function (data) {
+        var date;
+        data.forEach(function (d) {
+            //console.log(d.Tijd + "test");
+            d.Tijd += ":00";
+            d.glucose = d["Historie glucose (mg/dL)"];
+            //console.log(d.glucose + "test2");
+            if (d.Tijd.substring(0, 2).includes("/")) {
+                d.Tijd = "0" + d.Tijd;
+            }
+            var dateString = d.Tijd.substr(3, 2) + "/" + d.Tijd.substr(0, 2) + "/" + d.Tijd.substr(6, 4);
+            //console.log(dateString + "hello");
+            var timeStamp = moment(dateString).unix();
+            //timeStamp = '"' + timeStamp + '"';
+            //timeStamp = timeStamp.replace(':', "test")
+            //console.log(timeStamp + "whuut");
+            stats[timeStamp] = d.glucose;
+            //alert(ts);
+        });
+        //console.log(stats[1] + "help2");
+        //console.log(stats);
+        return init(stats);
+    })
+}
+
